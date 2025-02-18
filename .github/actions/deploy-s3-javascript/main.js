d554bdf944522afe3aa57ec9fc67d4b7b32fb8d7
luisfuentech <luisfuentes.au@gmail.com>
2025-02-18 00:51:12 -0500

    feat: enhance S3 deployment action with required bucket region input and update deployment configuration

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,11 +1,14 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    const bucketName = core.getInput('bucket-name', { required: true })
-    const bucketRegion = core.getInput('bucket-region')
-    const sourceDir = core.getInput('source-dir', { required: true })
+    const bucketName = core.getInput('bucket-name', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const sourceDir = core.getInput('source-dir', { required: true });
+
+    const s3Uri = `s3://${bucketName}`;
+    exec.exec(`aws s3 sync ${sourceDir} ${s3Uri} --region ${bucketRegion}`);
 
     core.notice('Hello from my custom JavaScript Action', { bucketName, bucketRegion, sourceDir });
 }
