7abcfc54dcdbc728af29e490dacf4741d75f1b8e
luisfuentech <luisfuentes.au@gmail.com>
2025-02-18 00:36:13 -0500

    feat: add input parameters for S3 bucket deployment action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,11 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello from my custom JavaScript Action');
+    const bucketName = core.getInput('bucket-name', { required: true })
+    const bucketRegion = core.getInput('bucket-region')
+    const sourceDir = core.getInput('source-dir', { required: true })
+
+    core.notice('Hello from my custom JavaScript Action', { bucketName, bucketRegion, sourceDir });
 }
 
 run()
